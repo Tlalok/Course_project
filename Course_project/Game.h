07f91e9c03176b24e30d532fa239055578e936c5
Egor Romanenko <egoruch96@gmail.com>
2014-10-18 23:20:19 +0300

    game

--- Course_project/Game.h
+++ Course_project/Game.h
@@ -9,6 +9,10 @@ class Game
     std::vector<Question> questions;
     // список всех ответов
     std::vector<Answer> answers;
+	//текущее кол-во заданных в этой игре вопросов
     // текущая последовательность вопросов и ответов которые дал пользователь
     std::vector<std::pair<unsigned int, unsigned int>> currentQustionsAnswers;
+	//здесь должен быть какой-то массив вероятностей текущих для всех персонажей, насколько я понимаю
+	//
+	//
 };
